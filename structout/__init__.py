64097f52c9913c774861426a110666f03a7b3d23
S Mautner <mautner@cs.uni-freiburg.de>
2021-07-22 06:59:03 +0000

    hist function

--- structout/__init__.py
+++ structout/__init__.py
@@ -6,3 +6,13 @@ from structout.intlist import lprint, dprint, npprint
 
 def bins(values,minmax=True,length=-1,method='bins',methodhow=16,**kwargs):
     npprint(values,minmax=minmax,length=length,method=method,methodhow=methodhow,**kwargs)
+
+
+
+def hist(values):
+    from collections import Counter
+    counts = Counter(values)
+    k=counts.keys()
+    val  = [counts.get(i,0) for i in range(min(k), max(k)+1)]
+    lprint(val)
+
