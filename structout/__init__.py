df2469fe3c26e49aa869af7f5589cf82a15040ab
S Mautner <mautner@cs.uni-freiburg.de>
2022-03-30 13:37:32 +0000

    new heatmap

--- structout/__init__.py
+++ structout/__init__.py
@@ -2,7 +2,8 @@
 
 from structout.graph import gprint
 from structout.intlist import dprint
-from structout.intlistV2 import lprint, npprint, iprint,heatmap
+from structout.intlistV2 import lprint, npprint, iprint
+from structout.heatmap import heatmap
 
 
 #def bins(values,minmax=True,length=-1,method='bins',methodhow=16,**kwargs):
