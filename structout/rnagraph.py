757dc2a6e81754442b9fdd8cac5093f3126deaba
S Mautner <mautner@cs.uni-freiburg.de>
2023-05-26 14:57:05 +0900

    rna printing

--- structout/rnagraph.py
+++ structout/rnagraph.py
@@ -1,55 +1,25 @@
 from lmz import Map,Zip,Filter,Grouper,Range,Transpose,Flatten
-
-
-import RNA
 from collections import defaultdict
 import networkx as nx
 import structout as so
+import numpy as np
 
 
 
 
-
-
-
-
-
-def normalRNA(struct):
-
+def getvienna_pos(struct):
+    '''alternative method... '''
+    import RNA
     rna_object = RNA.get_xy_coordinates(struct)
     pos = {i: (rna_object.get(i).X, rna_object.get(i).Y)
                for i in range(len(struct))}
     return pos
 
 
+def _sequence_dotbracket_to_graph(seq_info=None, seq_struct=None):
+    """
+    Given a sequence and the dotbracket sequence make a graph. useful for testing.
 
-
-
-def rnaprint(g, struct):
-    rna_object = RNA.naview_xy_coordinates(struct)
-    rna_object = RNA.simple_xy_coordinates(struct)
-
-    pos = {i: (rna_object[i].X, rna_object[i].Y)
-               for i in range(len(struct))}
-
-    print(pos)
-    # pos = normalRNA(struct) # this is the normal way
-    so.gprint(g,pos=pos, size = 40)
-    so.gprint(g,pos=pos, size = 40)
-
-
-
-
-def newcoo(g):
-
-    # greedily assign coordinates
-
-    breakpoint()
-    #
-
-
-def sequence_dotbracket_to_graph(seq_info=None, seq_struct=None):
-    """Given a sequence and the dotbracket sequence make a graph.
     Parameters
     ----------
     seq_info string
@@ -61,9 +31,7 @@ def sequence_dotbracket_to_graph(seq_info=None, seq_struct=None):
         returns a nx.Graph
         secondary struct associated with seq_struct
     """
-
     graph = nx.Graph()
-
     lifo = defaultdict(list)
     open_brace_string={")":"(",
                 "]":"[",
@@ -81,98 +49,9 @@ def sequence_dotbracket_to_graph(seq_info=None, seq_struct=None):
 
 
 
-
-
-def doit(se,st):
-
-    # 1. make a tree structure
-    lifo  = []
-
-    blocks = []
-    mode = 0
-    cblock= 0
-    for i,c in enumerate(st):
-        if c == f'(':
-            if mode == 1:
-                   blocks.append(cblock)
-            mode =  0
-            lifo.append(i)
-
-        if c == ')':
-            # switch mode...
-            if mode == 0:
-                mode = 1
-                # if cblock: blocks.append(cblock)
-                cblock = (lifo.pop(),i)
-            elif mode == 1:
-                cblock = (lifo.pop(),i)
-
-        print(i,mode,c,cblock, blocks)
-    blocks.append(cblock)
-    print(blocks)
-
-def doit2(seq,stu):
-    pairs = []
-    lifo =[]
-    for i,e in enumerate(stu):
-        if  e ==f'(':
-                   lifo.append(i)
-        elif e == f')':
-            pairs.append((lifo.pop(),i))
-    conn = dict(pairs)
-    conn.update({a:b for b,a in pairs})
-    first = 0
-    last = len(stu)
-
-    return getnode(first,last,stu,conn)
-
-def getnode(start,end,stu,conn):
-    left = start
-    right  = end
-    #......(....).....(...)...
-    children = []
-    while left < right:
-        print(f"{left=}")
-        print(f"{right=}")
-        if stu[left] == f'.':
-            left+=1
-        if stu[left] == f'(':
-            if f')' not in stu[conn[left]+1: right]:
-                print(f"{ conn=}")
-                right = conn[left]
-                left +=1
-            else:
-                children+=getnode(left,conn[left],stu,conn)
-                left = conn[left]+1
-                print(f"{ left=}{conn[left]}")
-    return (left,right, start,end, children)
-
-
-
-# from pyparsing import Word, alphas, Regex, OneOrMore
-# unpaired = Regex(f'.*')
-# left = Regex(f'[(.]*')
-# right = Regex(f'[).]*')
-# hairpin  = left + unpaired + right
-# ml = OneOrMore(stem)
-# stem  = ml | hairpin
-# seed =  unpaired | stem
-# print(greet.parseString(stu))
-
-from lark import Lark
-
-def getconn(seq,stu):
-    pairs = []
-    lifo =[]
-    for i,e in enumerate(stu):
-        if  e ==f'(':
-                   lifo.append(i)
-        elif e == f')':
-            pairs.append((lifo.pop(),i))
-    conn = dict(pairs)
-    conn.update({a:b for b,a in pairs})
-    return conn
-
+########################
+# setting up grammar and direction translations
+#######################
 grammar = '''
 start:  ( L start R ) *
 L: /\(/
@@ -181,131 +60,158 @@ R: /\)/
 %ignore ","
 '''
 
+# if the current orientation is KEY, and we see a multiloop, first child goes to value[0], etc
+treesplit = { f'R':f"URD",
+                f'U':f"LUR",
+                f'D':f"LDR" }
+# if the current direction is key, direction of closing bracket is value
+close = dict("RD UR DL".split())
+# reverse direction
+rev = dict("RL UD DU LR".split())
+
+# move into direction
+def newpos(pos, d):
+    if d == f'R':
+        return pos[0]+1, pos[1]
+    if d == f'U':
+        return pos[0], pos[1]-1
+    if d == f'D':
+        return pos[0], pos[1]+1
+    if d == f'L':
+        return pos[0]-1, pos[1]
+    else:
+        assert False
+
+
+###################################
+# make the possition dict...
+####################################
+
+def getposdict(blob,d, pos, duty):
+    '''
+    recursively the tree to get a posdict containing all nodes
+
+    blob:  is ( S ) ... so we can draw the brackets and the relevant company
+        and check the ccardinality of S
+    '''
+    r = {}
+
+    ########################
+    # draw terminals in    "...( S )..."; the grammar checks for brackets only,
+    # but we just draw the unpaired nucleotides until then too...
+    #########################
+    # adjust start possition... to accomodate left and right duty
+    # breakpoint()
+    leftlen = blob[0].start_pos - duty[0]
+    rightlen = duty[1] - blob[2].start_pos
+    diff = min(leftlen - rightlen +1 ,0)  # i assume the +1 compensates for a leftlenrightlen offby one...
+    for i in range(abs(diff)):
+        pos = newpos(pos,d)
+
+    # shit unpaired left
+    for i in range(duty[0], blob[0].start_pos):
+        pos = newpos(pos,d)
+        r[i] = pos
+
+    # shit bracket
+    pos = newpos(pos,d)
+    r[blob[0].start_pos] =  pos
+    # shit closing bracked
+    waybackpos = newpos(pos,close[d])
+    r[blob[2].start_pos] =  waybackpos
+
+    # shit unpaired left
+    for i in range(blob[2].start_pos+1,duty[1]):
+        waybackpos = newpos(waybackpos,rev[d])
+        r[i] = waybackpos
+
+
+    #############################
+    # here we take care of all nesting structures: stem, multiloop
+    ###########################
+    children = Grouper(blob[1].children,3)
+    targets = treesplit[d]
+    if len(children) == 1:
+        r.update(getposdict(children[0],d,pos,(blob[0].start_pos+1, blob[2].start_pos)))
+
+    elif len(children) == 2:
+        r.update(getposdict(children[0],targets[0],pos,(blob[0].start_pos+1, children[1][0].start_pos)))
+        r.update(getposdict(children[1],d,pos,(children[1][0].start_pos, blob[2].start_pos)))
+
+    elif len(children) == 3:
+        r.update(getposdict(children[0],targets[0],pos ,(blob[0].start_pos+1, children[1][0].start_pos)))
+        r.update(getposdict(children[1],d,pos,(children[1][0].start_pos+1, children[2][0].start_pos)))
+        r.update(getposdict(children[2],targets[2], newpos(newpos(pos,d),targets[2]),
+                                                        (children[2][0].start_pos, blob[2].start_pos)))
+    else:
+        ############
+        # unpaired nodes in hairpin,
+        # originally i wanted to place them with graphviz but that looks shitty
+        ###############
+        pos = newpos(pos,d)
+        pos = newpos(pos,targets[0])
+        for i in range(blob[0].start_pos+1,blob[2].start_pos):
+            r [i] =  pos
+            pos = newpos(pos,close[d])
+
+    return r
+
+
+def RNAprint(g,structure = None):
+    ###
+    # get structure annotation right:
+    #########
+    stu = g.graph.get(f'structure', structure)
+    if stu == None:
+        assert False, f'no structure in {structure=} {g.graph}'
+    def nani(ch):
+        if ch in f'([<':
+            return f'('
+        if ch in f')]>':
+            return f')'
+        return f','
+    stu = Map(nani, stu)
+
+    ####
+    # calculate a possition for every character in the strcuture
+    ########
+    from lark import Lark
+    p = Lark(grammar)
+    z = p.parse(stu)
+    # print(p.parse(stu).pretty())
+    posdict = getposdict(z.children,f'R',(0,0), (0, len(stu)))
+
+
+    #####
+    # undocumented nodes get filled by spring layout:
+    ########
+    pos = nx.drawing.spring_layout(g, pos =posdict ,fixed = posdict.keys())
+
+    ##########
+    # making sure the graph proportions are ok,  and draw
+    #############
+    a = np.array(list(pos.values()))
+    xmin,ymin = a.min(axis = 0)
+    xmax,ymax = a.max(axis = 0)
+    xr = int(xmax-xmin)
+    yr = int(ymax-ymin)
+
+    so.gprint(g, pos = pos, size = (xr*4,yr*2) )
+
+
+
+
+
 def test():
     seq = 'GACUCGACCUAGCGAGUAUAAACAGGCUUUAGGCUAGGAGCGUGACCACUUCGGUGGUCGGUAGCA'
     stu = '((((,,,),,,(,,,))),,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'
     seq = 'AAAAAAAAGGGGGGGGGGUUUUUUUUUUAAAAAAAAAAAA'
     stu = ',,,,((((,,((,,,)),,((,,,)),,((,,)))))),,'
-    conn = getconn(seq, stu)
+    g = _sequence_dotbracket_to_graph(seq, stu)
+    g.graph[f'structure'] = stu
 
-    p = Lark(grammar)
-    z = p.parse(stu)
-    print(p.parse(stu).pretty())
+    RNAprint(g)
 
-    pos = 0,0
-    g = sequence_dotbracket_to_graph(seq, stu)
-    posdict = {}
-
-
-    # rnaprint(g,stu)
-
-    # we want a pos dict: {nodeid:(x,y)}
-
-    treesplit = { f'R':f"URD",
-                    f'U':f"LUR",
-                    f'D':f"LDR" }
-
-    def newpos(pos, d):
-        if d == f'R':
-            return pos[0]+1, pos[1]
-        if d == f'U':
-            return pos[0], pos[1]-1
-        if d == f'D':
-            return pos[0], pos[1]+1
-        if d == f'L':
-            return pos[0]-1, pos[1]
-        else:
-            assert False
-    close = dict("RD UR DL".split())
-    rev = dict("RL UD".split())
-
-
-    def getposdict(blob,d, pos, duty):
-        '''
-        blob is ( S ) ... so we can draw the brackets and the relevant company
-            and check the ccardinality of S
-        '''
-        r = {}
-
-        # adjust start possition... to accomodate left and right duty
-        # breakpoint()
-        leftlen = blob[0].start_pos - duty[0]
-        rightlen = duty[1] - blob[2].start_pos
-        diff = min(leftlen - rightlen +1 ,0)  # i assume the +1 compensates for a leftlenrightlen offby one...
-        for i in range(abs(diff)):
-            pos = newpos(pos,d)
-
-        # shit unpaired left
-        for i in range(duty[0], blob[0].start_pos):
-            pos = newpos(pos,d)
-            r[i] = pos
-
-        # shit bracket
-        pos = newpos(pos,d)
-        r[blob[0].start_pos] =  pos
-        # shit closing bracked
-        waybackpos = newpos(pos,close[d])
-        r[blob[2].start_pos] =  waybackpos
-
-        # shit unpaired left
-        for i in range(blob[2].start_pos+1,duty[1]):
-            waybackpos = newpos(waybackpos,rev[d])
-            r[i] = waybackpos
-
-
-
-
-        children = Grouper(blob[1].children,3)
-
-
-        targets = treesplit[d]
-        if len(children) == 1:
-            r.update(getposdict(children[0],d,pos,(blob[0].start_pos+1, blob[2].start_pos)))
-
-        elif len(children) == 2:
-            r.update(getposdict(children[0],targets[0],pos,(blob[0].start_pos+1, children[1][0].start_pos)))
-            r.update(getposdict(children[1],d,pos,(children[1][0].start_pos, blob[2].start_pos)))
-
-        elif len(children) == 3:
-            r.update(getposdict(children[0],targets[0],pos ,(blob[0].start_pos+1, children[1][0].start_pos)))
-            r.update(getposdict(children[1],d,pos,(children[1][0].start_pos+1, children[2][0].start_pos)))
-            r.update(getposdict(children[2],targets[2], newpos(newpos(pos,d),targets[2]),
-                                                            (children[2][0].start_pos, blob[2].start_pos)))
-        else:
-            pos = newpos(pos,d)
-            pos = newpos(pos,treesplit[d][0])
-            for i in range(blob[0].start_pos+1,blob[2].start_pos):
-                r [i] =  pos
-                pos = newpos(pos,close[d])
-
-        return r
-
-
-
-
-
-
-    posdict = getposdict(z.children,f'R',pos, (0, len(stu)))
-
-
-    # posdict = {k:(v[1],v[0]) for k,v in posdict.items()}
-
-    pos = nx.drawing.spring_layout(g, pos =posdict ,fixed = posdict.keys())
-    so.gprint(g, pos = pos, size = (60,15) )
-
-
-
-
-
-
-
-
-
-
-
-    # g = sequence_dotbracket_to_graph(seq,stu)
-
-    #doit2(seq,stu)
 
 
 
